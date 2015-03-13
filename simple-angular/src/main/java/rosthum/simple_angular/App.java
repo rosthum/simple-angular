cf96ddf154b0558a0b255c52de7e4dace774b83e
rmouissette <rosthum@gmail.com>
2015-03-13 16:57:07 +0000

    1st version

---
+++ simple-angular/src/main/java/rosthum/simple_angular/App.java
@@ -0,0 +1,13 @@
+package rosthum.simple_angular;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ simple-angular/src/main/java/rosthum/simple_angular/SimpleController.java
@@ -0,0 +1,22 @@
+package rosthum.simple_angular;
+
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@EnableAutoConfiguration
+public class SimpleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SimpleController.class, args);
+    }
+}
